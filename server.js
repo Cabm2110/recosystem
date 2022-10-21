5b59a0168d5ca3ea98249c069296d6286f65725c
carlos_bojorquez <carlos.bojorquez@gmail.com>
2022-10-21 17:16:25 -0600

    deploy angular recosystem

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('.recosystem'));
 
-app.get('/', (req, res) =>
+app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: __dirname}),
 );
 
