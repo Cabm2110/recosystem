e876dd24211798d7f1eed70b03a4e32e46c8cacc
carlos_bojorquez <carlos.bojorquez@gmail.com>
2022-10-22 11:34:48 -0600

    deploy angular recosystem

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('.recosystem'));
+app.use(express.static(__dirname, '/dist/heroes-app'));
 
 app.get('/*', (req, res) =>
-    res.sendFile(path.resolve('././index.html')),
+    res.sendFile(path.join(__dirname, '/dist/heroes-app', 'index.html')),
 );
 
 // Start the app by listening on the default Heroku port
